9fabc7cd4ea4208792b926e6691c9be86f0e1485
ahmedmunaf123 <a.raza27.ar@gmail.com>
2018-02-18 22:48:53 +0500

    new adidition for compus app

--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -1,29 +1,69 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Header } from "./Header";
-import { Home } from "./Home";
-
-
+import React from 'react' ; 
+import {render} from "react-dom";
+import {Router, Route} from "react-router";
 class App extends React.Component {
     render() {
-        const user = {
-            name: "Ahmed Raza" ,
-            age: 18,
-            hobbies: ["Reding Quran","Work in Bazmeahlesunnat","Working as a developer"]
-        };
-        return (
-            <div>
-                <div>
-                    <Header />
-                </div>
-                <div>
-                    <Home user={user} name={user.name} age={user.age} greet={(greet) => {alert("Hellow")}} />
-                </div>
-            </div>
-
-        );
-    }
+        <Router>
+            <Route  />
+        </Router>
+    };
 }
+render(<App/>, window.document.getElementById("app"));
+// var nameref =  document.getElementById("username");
+// var emailref =  document.getElementById("useremail");
+// var pswref =  document.getElementById("userpsw");
+// var psw_repeatref =  document.getElementById("userpsw-repeat");
+// var customError = document.getElementById("custom-error");
 
-render(<App />, document.getElementById('root'));
 
+// function signup() {
+//     var user = {
+//         name  : nameref.value,
+//         email : emailref.value,
+//         psw   : pswref.value,
+//         psw_repeat   : psw_repeatref.value,
+//     }
+
+//    // console.log('user' , user);
+
+//    var valid = customValidation(user);
+//    if(valid) {
+//        console.log("valid" , valid);
+//        firebase.auth().createUserWithEmailAndPassword(user.email , user.psw)
+//        .then(function(success){
+//            console.log("success" , success);
+//        })
+//        .catch(function(error){
+//            console.log("error" , error);
+//            customError.style.display = "block";
+//            setTimeout(()=>{
+//                customError.style.display = "none";
+//            }, 3500)
+//        })
+//     }
+//     else{
+//         console.log("unvalid" , valid);
+//         customError.style.display = "block";
+//         setTimeout(()=>{
+//             customError.style.display = "none";
+//         }, 3500)
+//     } 
+
+
+
+// }
+
+// function customValidation(_user) {
+//     var valid = true;
+//     for (var key in _user ) {
+//         if (_user[key] === "" || _user[key] === undefined) {
+//             valid = false;
+//         }
+//     }
+
+//     if(_user.psw !== _user.psw_repeat ){
+//         valid = false;
+//     }
+//     return valid;
+
+// }
